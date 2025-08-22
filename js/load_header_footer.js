a2e8aae19e789e4ac7611c3a58f3b1a8db66e5b0
unknown <khang.minh0853854954@gmail.com>
2025-08-22 11:54:42 +0700

    Update webD

--- js/load_header_footer.js
+++ js/load_header_footer.js
@@ -1,10 +1,11 @@
-
+ // Tải header
     fetch("header.html")
       .then(res => res.text())
       .then(data => {
         document.getElementById("header").innerHTML = data;
       });
-   
+       // Gắn sự kiện sau khi header đã được chè  
+    // Tải footer
     fetch("footer.html")
       .then(res => res.text())
       .then(data => {
